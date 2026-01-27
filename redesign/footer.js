6af7e54d201f674df5238b4ae30ccc0d559ffe9e
Andrew Zambazos <andrewzambazos@gmail.com>
2026-01-27 13:36:11 +1300

    Add dynamic footer and update HTML includes
    Introduces footer.js to dynamically set the copyright year in the footer across all pages. Updates all HTML files to include the new footer.js script and removes unused favicon link tags for consistency.

---
+++ redesign/footer.js
@@ -0,0 +1,6 @@
+
+  const footerText = document.querySelector("footer p");
+  const year = new Date().getFullYear();
+
+  footerText.innerHTML = `&copy; ${year} Andrew Zambazos. All Rights Reserved.`;
+
